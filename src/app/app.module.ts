9b3aefdb1b0d505445822c4a8524530075404e22
Douglas  Monteiro Gomes <re039513@resource.com.br>
2020-07-15 17:44:33 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { HomeComponent } from './pages/home/home.component';
+import { MatIconModule } from '@angular/material/icon/icon-module';
+import { MatToolbarModule } from '@angular/material/toolbar/toolbar-module';
+import { MatButtonModule } from '@angular/material/button/button-module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
